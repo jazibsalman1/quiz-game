b01aa48f1f2d799599c36c201545f2efc09f5d1f
jazibsalman1 <jazib1341@gmail.com>
2026-06-04 13:24:28 +0500

    fix

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from fastapi import FastAPI ,requests
-
+﻿from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+
 app = FastAPI()
+
+
+
+# Static (CSS/JS/Images)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Templates (HTML files)
+templates = Jinja2Templates(directory="templates")
+
+
+
+# default route
+@app.get("/", response_class=HTMLResponse)
+async def index_page(request: Request):
+    return templates.TemplateResponse(request, "index.html", {})
+
+# Run the app
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
